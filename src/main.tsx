eeafee1debcb162398634e05c28b4a1923c31bb0
r-southworth <rachael.southworth@datagrove.com>
2022-10-27 17:38:06 -0400

    Try to get it to refresh state on exit of search box

--- src/main.tsx
+++ src/main.tsx
@@ -41,7 +41,7 @@ let view = new EditorView(editor, {
 
 let search = document.querySelector('#search') as HTMLInputElement;
 let replace = document.querySelector('#replace') as HTMLInputElement;
-/*
+
 document.getElementById('go')?.addEventListener('click', () => {
   let s = editor.querySelector(".ProseMirror")!.innerHTML
   content.innerHTML = s.replaceAll(search.value, replace.value)
@@ -50,9 +50,9 @@ document.getElementById('go')?.addEventListener('click', () => {
     plugins: exampleSetup({ schema: mySchema })
   }))
 })
-*/
 
-document.getElementById('search')?.addEventListener('change', () => {
+
+document.querySelector('search')?.addEventListener('change', () => {
   let s = editor.querySelector(".ProseMirror")!.innerHTML
   content.innerHTML = s
   view.updateState(EditorState.create({
@@ -60,115 +60,4 @@ document.getElementById('search')?.addEventListener('change', () => {
     plugins: exampleSetup({ schema: mySchema })
   }))
 })
-
-/*
-// Words you probably shouldn't use
-const badWords = /\b(obviously|clearly|evidently|simply)\b/ig
-// Matches punctuation with a space before it
-const badPunc = / ([,\.!?:]) ?/g
-
-function lint(doc: Node) {
-  let result: { msg: string; from: number; to: number; fix: Function | null; }[]= [], lastHeadLevel: number|null = null
-
-  function record(msg: string, from: number, to: number, fix: number|null) {
-    result.push({msg, from, to, fix})
-  }
-
-  // For each node in the document
-  doc.descendants((node: Node, pos: number, parent: Node|null ) => {
-    if (node.isText) {
-      // Scan text nodes for suspicious patterns
-      let m
-      while (m = badWords.exec(node.text!))
-        record(`Try not to say '${m[0]}'`,
-               pos + m.index, pos + m.index + m[0].length, null)
-      while (m = badPunc.exec(node.text!))
-        record("Suspicious spacing around punctuation",
-               pos + m.index, pos + m.index + m[0].length,
-               fixPunc(m[1] + " "))
-    } else if (node.type.name == "heading") {
-      // Check whether heading levels fit under the current level
-      let level = node.attrs.level
-      if (lastHeadLevel != null && level > lastHeadLevel + 1)
-        record(`Heading too small (${level} under ${lastHeadLevel})`,
-               pos + 1, pos + 1 + node.content.size,
-               fixHeader(lastHeadLevel + 1))
-      lastHeadLevel = level
-    } else if (node.type.name == "image" && !node.attrs.alt) {
-      // Ensure images have alt text
-      record("Image without alt text", pos, pos + 1, addAlt(pos))
-    }
-  })
-
-  return result
-}
-
-function fixPunc(replacement: string) {
-  return function({state, dispatch}:{state:EditorState,dispatch:(tr: Transaction) => void}) {
-    dispatch(state.tr.replaceWith(from, to,
-                                  state.schema.text(replacement)))
-  }
-}
-
-function fixHeader(level: number) {
-  return function({state, dispatch}:{state:EditorState,dispatch:(tr: Transaction) => void}) {
-    dispatch(state.tr.setNodeMarkup(this.from - 1, null, {level}))
-  }
-}
-
-function addAlt(from: number, {state, dispatch}:{state:EditorState,dispatch:(tr: Transaction) => void}) {
-  let alt = prompt("Alt text", "")
-  if (alt) {
-    let attrs = Object.assign({}, state.doc.nodeAt(from).attrs, {alt})
-    dispatch(state.tr.setNodeMarkup(from, null, attrs))
-  }
-}
-
-function lintDeco(doc: Node) {
-  let decos: Decoration[]=[]
-  lint(doc).forEach(prob => {
-    decos.push(Decoration.inline(prob.from, prob.to, {class: "problem"}),
-               Decoration.widget(prob.from, lintIcon(prob)))
-  })
-  return DecorationSet.create(doc, decos)
-}
-
-function lintIcon(prob: { msg: any; from?: number; to?: number; fix?: Function | null; }) {
-  let icon = document.createElement("div")
-  icon.className = "lint-icon"
-  icon.title = prob.msg
-  icon.problem = prob
-  return icon
-}
-
-let lintPlugin = new Plugin({
-  state: {
-    init(_, {doc}) { return lintDeco(doc) },
-    apply(tr, old) { return tr.docChanged ? lintDeco(tr.doc) : old }
-  },
-  props: {
-    decorations(state) { return this.getState(state) },
-    handleClick(view, _, event) {
-      if (/lint-icon/.test(event.target!.className)) {
-        let {from, to} = event.target!.problem
-        view.dispatch(
-          view.state.tr
-            .setSelection(TextSelection.create(view.state.doc, from, to))
-            .scrollIntoView())
-        return true
-      }
-    },
-    handleDoubleClick(view, _, event) {
-      if (/lint-icon/.test(event.target!.className)) {
-        let prob = event.target!.problem
-        if (prob.fix) {
-          prob.fix(view)
-          view.focus()
-          return true
-        }
-      }
-    }
-  }
-})
-*/
 
